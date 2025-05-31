470024f58092f8c94cd9afc240bde0efe20d72c5
Anisa Febriyani17 <anisa.febriyani17@gmail.com>
2025-05-31 23:46:24 +0700

    create : landing page

---
+++ Week7/final-project-anisa/final-project/src/pages/LandingPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function LandingPage() {
+  return (
+    <div>
+      <h1>IjoImut</h1>
+      <p>Bumi Ceria Mulai dari Kamu!</p>
+      <Link to="/catalog">
+        <button>Lihat Produk</button>
+      </Link>
+    </div>
+  );
+}
